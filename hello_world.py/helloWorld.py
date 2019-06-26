64f5f4afd784ff6a892bcfc6889b311bf0d07c39
Sloka Sudhin <sloka.sudhin@wheelermagnet.com>
2019-06-26 14:12:04 -0400

    added gitignore, did some Pycharm

---
+++ hello_world.py/helloWorld.py
@@ -0,0 +1,15 @@
+"""
+title: helloWorld
+author: mfp4311
+date: 6/25/19 2:08 PM
+"""
+
+print("Elizabeth,", end=" ")
+print("Emily,", end=" ")
+print("Bob,", end=" ")
+print("Antonio")
+
+print("\t\t\t\t\t|||")
+print("\t\t\t\t\t O")
+print("\t\t\t\t   /   \ ")
+print("\t\t\t\t\t| |")
